f290d9114f557f012aca99644296b4f3d7f8eb44
xervir <nchietala@gmail.com>
2016-12-20 16:17:43 -0500

    install script fix
    bug  where mount directory did not exist
    bug where start script was incorrectly named and would not start

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 # put all the files where they go
 wget -O /home/pi/sensor.py https://raw.githubusercontent.com/xervir/rpi-sensor-controller/master/source/sensor.py
-wget -O /etc/init.d/startsensor https://raw.githubusercontent.com/xervir/rpi-sensor-controller/master/source/sensorstart
+wget -O /etc/init.d/sensorstart https://raw.githubusercontent.com/xervir/rpi-sensor-controller/master/source/sensorstart
 wget -O /home/pi/LICENSE.md https://raw.githubusercontent.com/xervir/rpi-sensor-controller/master/LICENSE.md
 wget -O /home/pi/README.md https://raw.githubusercontent.com/xervir/rpi-sensor-controller/master/README.md
 
@@ -18,6 +18,8 @@ update-rc.d sensorstart defaults
 # install screen and python, python because the script runs on it, and screen because it makes it easier to
 # to ensure that the script can be easily monitored without accidentally killing the session it runs in
 apt-get -y install screen python
+
+mkdir /mnt/sda
 
 #clean up
 rm install.sh
