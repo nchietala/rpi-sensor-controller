8c5a08443daf8804f2261bbbc965793538a19004
xervir <nchietala@gmail.com>
2017-09-13 14:04:41 -0400

    Update install.sh
    Added line to reset controller after install, removed the clean-up. The clean up is unnecessary. Changed all the wget commands to curl so that the same file can be used as an updater, not just an installer.

--- install.sh
+++ install.sh
@@ -2,11 +2,15 @@
 # /home/pi/install.sh
 # install script for multi-channel input sensor
 
+#update all the things
+apt-get --yes update
+apt-get --yes upgrade
+
 # put all the files where they go
-wget -O /home/pi/sensor.py https://raw.githubusercontent.com/xervir/rpi-sensor-controller/master/source/sensor.py
-wget -O /etc/init.d/sensorstart https://raw.githubusercontent.com/xervir/rpi-sensor-controller/master/source/sensorstart
-wget -O /home/pi/LICENSE.md https://raw.githubusercontent.com/xervir/rpi-sensor-controller/master/LICENSE.md
-wget -O /home/pi/README.md https://raw.githubusercontent.com/xervir/rpi-sensor-controller/master/README.md
+curl https://raw.githubusercontent.com/xervir/rpi-sensor-controller/master/source/sensor.py > /home/pi/sensor.py
+curl https://raw.githubusercontent.com/xervir/rpi-sensor-controller/master/source/sensorstart > /etc/init.d/sensorstart
+curl https://raw.githubusercontent.com/xervir/rpi-sensor-controller/master/LICENSE.md > /home/pi/LICENSE.md
+curl https://raw.githubusercontent.com/xervir/rpi-sensor-controller/master/README.md > /home/pi/README.md
 
 # make the necessary files executable, readable, and writeable
 chmod 777 /etc/init.d/sensor.py
@@ -17,9 +21,9 @@ update-rc.d sensorstart defaults
 
 # install screen and python, python because the script runs on it, and screen because it makes it easier to
 # to ensure that the script can be easily monitored without accidentally killing the session it runs in
-apt-get -y install screen python
+apt-get --yes --force-yes install screen python
 
 mkdir /mnt/sda
 
-#clean up
-rm install.sh
+# reboot the machine
+sudo shutdown -r 1 "The installation is complete, the controller will now reboot. Make sure sensor leads and switch are installed."
